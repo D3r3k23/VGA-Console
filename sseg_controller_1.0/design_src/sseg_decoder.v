d7773e80a7f516820b03d9ad2360a280daeb3bee
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2020-12-29 21:02:19 -0800

    Create sseg_decoder.v

---
+++ sseg_controller_1.0/design_src/sseg_decoder.v
@@ -0,0 +1,38 @@
+
+module sseg_decoder
+(
+    input  wire [13 : 0] num,
+    input  wire [1  : 0] dig_sel,
+    output wire [3  : 0] an_data,
+    output reg  [6  : 0] cat_data
+);
+    
+    assign an_data = 1'b1 << dig_sel;
+
+    reg [3 : 0] dig_data;
+
+    always @*
+        case (dig_sel)
+            2'd0 : dig_data = (num / 1) % 10;
+            2'd1 : dig_data = (num / 10) % 10;
+            2'd2 : dig_data = (num / 100) % 10;
+            2'd3 : dig_data = (num / 1000) % 10;
+         default : dig_data = 0;
+        endcase
+
+    always @*
+        case (dig_data)
+            4'd0 : cat_data = 7'b0111111;
+            4'd1 : cat_data = 7'b0000110;
+            4'd2 : cat_data = 7'b1011011;
+            4'd3 : cat_data = 7'b1001111;
+            4'd4 : cat_data = 7'b1100110;
+            4'd5 : cat_data = 7'b1101101;
+            4'd6 : cat_data = 7'b1111101;
+            4'd7 : cat_data = 7'b0000111;
+            4'd8 : cat_data = 7'b1111111;
+            4'd9 : cat_data = 7'b1100111;
+         default : cat_data = 0;
+        endcase
+
+endmodule // sseg_decoder
