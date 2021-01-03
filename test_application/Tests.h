fc98c34c94f859cf9f3eaf4ffc522abf1076e8be
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2021-01-03 03:03:58 -0800

    Update Tests.h

--- test_application/Tests.h
+++ test_application/Tests.h
@@ -1,6 +1,8 @@
 
+#ifndef TESTS_H
+#define TESTS_H
+
 #include "VGA_Console.h"
-#include <stdlib.h>
 #include <time.h>
 
 
@@ -12,6 +14,7 @@ private:
 public:
     static void HelloWorld()
     {
+        console.set_char_color(Color::GREEN);
         console.puts("Hello World!\n");
     }
 
@@ -38,4 +41,81 @@ public:
         console.endl();
         console.puts("Final line.");
     }
+
+    static void Overflow()
+    {
+        console.puts("Line 1");
+        console.set_cursor(72, 1);
+        console.puts("Text wrapping test.");
+        console.endl();
+        console.endl();
+        console.puts("Second line.");
+        console.set_cursor(74, 43);
+        console.puts("1234567890");
+    }
+
+    static void Colors()
+    {
+        console.set_back_color(Color::WHITE);
+
+        console.set_char_color(Color::BLACK);
+        console.puts("Black\n");
+        console.set_char_color(Color::RED);
+        console.puts("Red\n");
+        console.set_char_color(Color::GREEN);
+        console.puts("Green\n");
+        console.set_char_color(Color::BLUE);
+        console.puts("Blue\n");
+    }
+
+    static void FillScreen(char fillChar='E')
+    {
+        for (int i = 0; i < 45; i++)
+            for(int j = 0; j < 80; j++)
+            {
+                console.set_cursor(j, i);
+                console.putc(fillChar);
+            }
+    }
+
+    static void Clearing()
+    {
+        console.puts("Hello\n\n");
+        console.puts("Goodbye");
+        sleep(8);
+        console.clr_line();
+        console.endl();
+        console.endl();
+        console.puts("Goodbye");
+        sleep(4);
+        console.clr_screen();
+        console.puts("Goodbye");
+    }
+
+    static void Count()
+    {
+        sleep(4);
+        for (int i = 1; i <= 43; i++)
+        {
+            console.puts("Line: ");
+            console.puti(i);
+            console.endl();
+            sleep(1);
+        }
+    }
+
+    static void HelloHelloWorld()
+    {
+        console.puts("#include <stdio.h>\n");
+        console.puts("\n");
+        console.puts("int main(void)\n");
+        console.puts("{\n");
+        console.puts("    printf(\"Hello World!\");\n");
+        console.puts("\n");
+        console.puts("    return 0;\n");
+        console.puts("}\n");
+    }
 };
+
+
+#endif // TESTS_H
