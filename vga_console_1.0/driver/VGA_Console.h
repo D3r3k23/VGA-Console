0fd375408066a6c4123ed5fda7cef2b68250034a
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2021-01-03 02:51:26 -0800

    Update VGA_Console.h

--- vga_console_1.0/driver/VGA_Console.h
+++ vga_console_1.0/driver/VGA_Console.h
@@ -3,28 +3,32 @@
 #define VGA_CONSOLE_H
 
 #include <stdint.h>
+#include <stdlib.h>
+#include <sleep.h>
 #include "xparameters.h"
+
 
 const uint32_t CONS_BASE = XPAR_VGA_CONSOLE_0_S_AXI_BASEADDR;
 
-#define CONS_CTRL  *((uint32_t*) (CONS_BASE + 0x0)) // Ctrl
-#define CURS_LOC   *((uint32_t*) (CONS_BASE + 0x4)) // Cursor location
-#define CHAR_SET   *((uint32_t*) (CONS_BASE + 0x8)) // Character
-#define CHAR_COLOR *((uint32_t*) (CONS_BASE + 0xC)) // Character color
-#define BACK_COLOR *((uint32_t*) (CONS_BASE + 0x0)) // Background color
+#define CONS_CTRL  *((uint32_t*) (CONS_BASE + 0x00)) // Ctrl
+#define CURS_LOC   *((uint32_t*) (CONS_BASE + 0x04)) // Cursor location
+#define CHAR_SET   *((uint32_t*) (CONS_BASE + 0x08)) // Character
+#define CHAR_COLOR *((uint32_t*) (CONS_BASE + 0x0C)) // Character color
+#define BACK_COLOR *((uint32_t*) (CONS_BASE + 0x10)) // Background color
 
 
 struct Point
     { int x, y; };
 
-enum Color
+struct Color
 {
-    BLACK = 0x000,
-    WHITE = 0xFFF,
-    RED   = 0xF00,
-    GREEN = 0x0F0,
-    BLUE  = 0x00F
+    const static uint16_t BLACK = 0x000;
+    const static uint16_t WHITE = 0xFFF;
+    const static uint16_t RED   = 0xF00;
+    const static uint16_t GREEN = 0x0F0;
+    const static uint16_t BLUE  = 0x00F;
 };
+
 
 class VGA_Console
 {
@@ -33,34 +37,34 @@ public:
     void en(void);
     void dis(void);
 
+    void putc(char ch);         // Prints character
+    void puts(const char* str); // Prints string
+    void puti(int num);         // Prints integer
+    void endl(void);            // Advances to new line
+
     void clr_line(void);
     void clr_screen(void);
 
-    void putc(char ch);         // Prints character
-    void puts(const char* str); // Prints string
-
-    void set_cursor(int x, int y);
     void set_cursor(Point point);
+    void set_cursor(int x, int y);
     Point get_cursor(void);
 
-    void set_char_color(Color color);
-    void set_back_color(Color color);
-
     void set_margin(int x, int y);
+
+    void set_char_color(uint16_t color);
+    void set_back_color(uint16_t color);
+
+private:
+    void enable_write(void);
+    void disable_write(void);
+    void write_char(char ch); // Writes the character to the cursor position
+    void inc_cursor(void);    // Increments cursor
 
 private:
     const int HEIGHT = 45;
     const int WIDTH  = 80;
 
-    bool displayEn;
     Point cursor, margin;
-    Color charColor, backColor;
-
-private:
-    void set_w_en(bool w_en); // Sets w_en bit of console control register
-    void write_char(char ch); // Sets character register
-    void inc_cursor(void);    // Increments cursor
-    void endl(void);          // Advances to new line
 };
 
 
