f9030fa6d95d769dbf00eba0570c3af29a3b4e34
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2021-01-03 02:51:16 -0800

    Update VGA_Console.cpp

--- vga_console_1.0/driver/VGA_Console.cpp
+++ vga_console_1.0/driver/VGA_Console.cpp
@@ -1,18 +1,19 @@
 
 #include "VGA_Console.h"
 
+
+////////// Public functions //////////
+
 VGA_Console::VGA_Console(void)
 {
-    CONS_CTRL = 0;
-    CHAR_SET  = 0;
+    disable_write();
+    dis();
 
-    // clr_screen();
+    set_cursor(1, 1);
+    set_margin(1, 1);
 
-    set_cursor(8, 4);
-    set_margin(8, 4);
-
-    set_char_color(WHITE);
-    set_back_color(BLACK);
+    set_char_color(Color::WHITE);
+    set_back_color(Color::BLACK);
     en();
 }
 
@@ -22,9 +23,43 @@ void VGA_Console::en(void)
 void VGA_Console::dis(void)
     { CONS_CTRL &= ~0b1; }
 
+void VGA_Console::putc(char ch)
+{
+    if (ch == '\n')
+        endl();
+    else
+    {
+        write_char(ch);
+        inc_cursor();
+    }
+}
+
+void VGA_Console::puts(const char* str)
+{
+    int i = 0;
+
+    while (str[i] != '\0')
+        putc(str[i++]);
+}
+
+void VGA_Console::puti(int num)
+{
+    char num_str[12];
+    itoa(num, num_str, 10);
+    puts(num_str);
+}
+
+void VGA_Console::endl(void)
+{
+    if (cursor.y == (HEIGHT - 1 - margin.y)) // Cursor on final line
+        return;
+    else
+        set_cursor(margin.x, cursor.y + 1);
+}
+
 void VGA_Console::clr_line(void)
 {
-    for (int i = margin.x; i <= WIDTH - margin.x; i++)
+    for (int i = 0; i < WIDTH; i++)
     {
         set_cursor(i, cursor.y);
         putc('\0');
@@ -34,100 +69,66 @@ void VGA_Console::clr_line(void)
 
 void VGA_Console::clr_screen(void)
 {
-    for (int i = margin.y; i <= HEIGHT - margin.y; i++)
+    for (int i = 0; i < HEIGHT; i++)
     {
         set_cursor(0, i);
         clr_line();
     }
-    set_cursor(margin.x, margin.y);
-}
-
-void VGA_Console::putc(char ch)
-{
-    if (ch == 0x12 || ch == 0x15)
-        endl();
-
-    set_w_en(false);
-    write_char(ch);
-    set_w_en(true);
-    // wait for resp?
-    set_w_en(false);
-    inc_cursor();
-}
-
-void VGA_Console::puts(const char* str)
-{
-    int i = 0;
-    while (1)
-    {
-        if (str[i] == '\0' || i == 50)
-            return;
-        else
-            putc(str[i]);
-        
-        i++;
-    }
-}
-
-void VGA_Console::endl(void)
-{
-    if (cursor.y == (HEIGHT - margin.y))
-        return;
-    else
-        cursor.y++;
-}
-
-void VGA_Console::set_cursor(int x, int y)
-{
-    if ((1 <= x && x <= WIDTH)
-     && (1 <= y && y <= HEIGHT))
-    {
-        CURS_LOC = ((x - 1) & 0xFFF);
-        CURS_LOC <<= 12;
-        CURS_LOC |= ((y - 1) & 0xFFF);
-
-        cursor = { x, y };
-    }
+    set_cursor(margin);
 }
 
 void VGA_Console::set_cursor(Point point)
-    { set_cursor(point.x, point.y); }
+{
+    if ((0 <= point.x && point.x < WIDTH ) && (0 <= point.y && point.y < HEIGHT))
+    {
+        cursor = point;
+
+        CURS_LOC = (cursor.x & 0xFFF);
+        CURS_LOC <<= 12;
+        CURS_LOC |= (cursor.y & 0xFFF);
+    }
+}
+
+void VGA_Console::set_cursor(int x, int y)
+    { set_cursor({ x, y }); }
 
 Point VGA_Console::get_cursor(void)
     { return cursor; }
 
-void VGA_Console::set_char_color(Color color)
-    { CHAR_COLOR = (uint16_t)color; }
-
-void VGA_Console::set_back_color(Color color)
-    { BACK_COLOR = (uint16_t)color; }
-
 void VGA_Console::set_margin(int x, int y)
 {
-    if (0 <= x && x <= WIDTH / 2)
+    if (0 <= x && x < WIDTH / 2)
         margin.x = x;
-    if (0 <= y && y <= HEIGHT / 2)
+    if (0 <= y && y < HEIGHT / 2)
         margin.y = y;
 }
 
-void VGA_Console::set_w_en(bool w_en)
-    { CONS_CTRL |= ((uint8_t)w_en << 1); }
+void VGA_Console::set_char_color(uint16_t color)
+    { CHAR_COLOR = (color & 0xFFF); }
+
+void VGA_Console::set_back_color(uint16_t color)
+    { BACK_COLOR = (color & 0xFFF); }
+
+
+////////// Private functions //////////
+
+void VGA_Console::enable_write(void)
+    { CONS_CTRL |= (0b1 << 1); }
+
+void VGA_Console::disable_write(void)
+    { CONS_CTRL &= ~(0b1 << 1); }
 
 void VGA_Console::write_char(char ch)
-    { CHAR_SET = ch; }
+{
+    CHAR_SET = ch;
+    enable_write();
+    disable_write();
+}
 
 void VGA_Console::inc_cursor(void)
 {
-    if (cursor.x == (WIDTH - margin.y))
-    {
-        if (cursor.y == (HEIGHT - margin.x))
-            return;
-        else
-        {
-            cursor.y++;
-            cursor.x = margin.x + 1;
-        }
-    }
+    if (cursor.x == (WIDTH - 1 - margin.x))
+        endl();
     else
-        cursor.x++;
+        set_cursor(cursor.x + 1, cursor.y);
 }
